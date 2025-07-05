866ccadc5cb867ca0be960e8b467b279ab133ebd
Safin <ilnursafin35@gmail.com>
2025-07-05 22:26:05 +0500

    first commit

---
+++ goal_go.go
@@ -0,0 +1,9 @@
+package main
+
+import ("fmt")
+
+func main() {
+    // int1 := 5
+    int2 := 5.0
+    fmt.Println(int(int2))
+}
---
+++ hello_go.go
@@ -0,0 +1,18 @@
+/* the result will be that very cute kitty** :
+
+/\_/\     (hello, Go!)
+>^,^<  __/
+ / \  
+(|_|)_/
+
+Thus I also learned how to screen correctly
+
+*/
+
+package main
+import "fmt"
+func main() {
+	var value string
+	value = "/\\_/\\    (hello, Go!) \n>^,^< __/ \n / \\ \n(|_|)_/"
+	fmt.Println(value)
+}
