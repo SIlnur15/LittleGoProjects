e9f7e0d175cf00c6772541ddfec411f1b041152e
Safin <ilnursafin35@gmail.com>
2025-07-21 20:51:26 +0500

    Happy's birthday

--- workspace/trash.go
+++ workspace/trash.go
@@ -1,27 +1,44 @@
 package main
 
-import (
-	"dates"
-	"fmt"
-	"log"
-)
+import "fmt"
+
+type myInterface interface {
+	PlayMusic(string)
+	StopMusic()
+}
+
+type Player string
+
+func (p Player) PlayMusic(sng string) {
+	fmt.Printf("it's time to play that very good song - %s\n", sng)
+}
+
+func (p Player) StopMusic() {
+	fmt.Println("stop music")
+}
+
+type Recorder string
+
+func (r Recorder) PlayMusic(sng string) {
+	fmt.Printf("it's time to play that very good song - %s\n", sng)
+}
+
+func (r Recorder) StopMusic() {
+	fmt.Println("stop music")
+}
+
+func Sonic(r myInterface, songs []string) {
+	for _, song := range songs {
+		r.PlayMusic(song)
+	}
+	r.StopMusic()
+}
 
 func main() {
-	interest := dates.Dates{}
-	err := interest.SetDay(22)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = interest.SetMonth(2)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = interest.SetYear(1994)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(interest)
-	fmt.Println(interest.Day())
-	fmt.Println(interest.Month())
-	fmt.Println(interest.Year())
+	player := Player("")
+	var experience myInterface = player
+	listOfSongs := []string{"mother", "i", "love"}
+	Sonic(experience, listOfSongs)
+	experience = Recorder("")
+	Sonic(experience, listOfSongs)
 }
