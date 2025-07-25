c8372d8ff8b493d2167aa7dc0ea1b539f3fbb903
Safin <ilnursafin35@gmail.com>
2025-07-26 00:04:45 +0500

    goroutines

--- workspace/trash.go
+++ workspace/trash.go
@@ -1,14 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
-func toPrint(mc chan int) {
-	mc <- 555
+type Page struct {
+	URL  string
+	Size int
+}
+
+func responseSize(url string, channel chan Page) {
+	fmt.Println("Getting", url)
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	channel <- Page{URL: url, Size: len(body)}
 }
 
 func main() {
-	var myChanel chan int
-	myChanel = make(chan int)
-	go toPrint(myChanel)
-	fmt.Println(<-myChanel)
+	pages := make(chan Page)
+	urls := []string{"https://example.com/", "https://golang.org/", "https://golang.org/doc"}
+	for _, url := range urls {
+		go responseSize(url, pages)
+	}
+	for i := 0; i < len(urls); i++ {
+		page := <-pages
+		fmt.Printf("%s: %d\n", page.URL, page.Size)
+	}
 }
