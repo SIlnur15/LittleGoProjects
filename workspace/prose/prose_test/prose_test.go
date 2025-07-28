b1e873696164b49007cd9a37d02b52cee14e8da1
Safin <ilnursafin35@gmail.com>
2025-07-29 02:16:59 +0500

    again testing

--- workspace/prose/prose_test/prose_test.go
+++ workspace/prose/prose_test/prose_test.go
@@ -5,29 +5,48 @@ import (
 	"testing"
 )
 
-func TestOneElement(t *testing.T) {
-	interest := []string{"apples"}
-	returned := prose.JoinWithCommas(interest)
-	wanted := "apples"
-	if returned != wanted {
-		t.Errorf("%s != %s", returned, wanted)
+type testData struct {
+	sending []string
+	wanted  string
+}
+
+func TestElements(t *testing.T) {
+	tests := []testData{
+		{sending: []string{"apple"}, wanted: "apple"},
+		{sending: []string{"apple", "pear"}, wanted: "apple and pear"},
+		{sending: []string{"apple", "pear", "banana"}, wanted: "apple, pear, and banana"},
+	}
+	for _, value := range tests {
+		got := prose.JoinWithCommas(value.sending)
+		if got != value.wanted {
+			t.Errorf("%s != %s", value.sending, value.wanted)
+		}
 	}
 }
 
-func TestTwoElements(t *testing.T) {
-	interest := []string{"apples", "pears"}
-	returned := prose.JoinWithCommas(interest)
-	wanted := "apples and pears"
-	if returned != wanted {
-		t.Errorf("%s != %s", returned, wanted)
-	}
-}
+// func TestOneElement(t *testing.T) {
+// 	interest := []string{"apples"}
+// 	returned := prose.JoinWithCommas(interest)
+// 	wanted := "apples"
+// 	if returned != wanted {
+// 		t.Errorf("%s != %s", returned, wanted)
+// 	}
+// }
 
-func TestThreeElements(t *testing.T) {
-	interest := []string{"apples", "pears", "bananas"}
-	returned := prose.JoinWithCommas(interest)
-	wanted := "apples, pears, and bananas"
-	if returned != wanted {
-		t.Errorf("%s != %s", returned, wanted)
-	}
-}
+// func TestTwoElements(t *testing.T) {
+// 	interest := []string{"apples", "pears"}
+// 	returned := prose.JoinWithCommas(interest)
+// 	wanted := "apples and pears"
+// 	if returned != wanted {
+// 		t.Errorf("%s != %s", returned, wanted)
+// 	}
+// }
+
+// func TestThreeElements(t *testing.T) {
+// 	interest := []string{"apples", "pears", "bananas"}
+// 	returned := prose.JoinWithCommas(interest)
+// 	wanted := "apples, pears, and bananas"
+// 	if returned != wanted {
+// 		t.Errorf("%s != %s", returned, wanted)
+// 	}
+// }
