72f4fa96c5209ef20a7f5742160727410369a375
Safin <ilnursafin35@gmail.com>
2025-08-20 00:54:37 +0500

    20.08.2025

--- workspace/trash.go
+++ workspace/trash.go
@@ -1,18 +1,18 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
 func main() {
-	var input1, input2, input3, input4 float64             // ф-я math.Pow требует такой тип
-	_, err := fmt.Scan(&input1, &input2, &input3, &input4) // считываем числа из os.Stdin
-	if err != nil {
-		fmt.Println("wrong entering:", err)
-		return
+	i := 1
+	fmt.Print("Write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+		fallthrough
+	case 2:
+		fmt.Println("two")
+		// fallthrough
+	case 3:
+		fmt.Println("Bye-bye")
 	}
-	under_sqrt := math.Pow(input1-input3, 2) + math.Pow(input2-input4, 2)
-	d := math.Pow(under_sqrt, 0.5) // attention 1/2 не покатит !!!
-	fmt.Println(d)
 }
