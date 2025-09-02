e531455671a80ed86ce21964696198f89da2d5fd
Safin <ilnursafin35@gmail.com>
2025-09-03 04:33:51 +0500

    3'8'2025

--- workspace/trash.go
+++ workspace/trash.go
@@ -1,29 +1,15 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
+import "fmt"
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	delimiter, _ := reader.ReadString('\n')
-	delimiter = strings.TrimSpace(delimiter)
-	s1, _ := reader.ReadString('\n')
-	s1 = strings.TrimSpace(s1)
-	s2, _ := reader.ReadString('\n')
-	s2 = strings.TrimSpace(s2)
-	s3, _ := reader.ReadString('\n')
-	s3 = strings.TrimSpace(s3)
-	fmt.Print(s1, delimiter, s2, delimiter, s3)
-}
+	var x int
+	fmt.Scan(&x)
 
-// func main() {
-// 	new_scanner := bufio.NewScanner(os.Stdin)
-// 	for new_scanner.Scan() {
-// 		name := new_scanner.Text()
-// 		fmt.Print(name)
-// 	}
-// }
+	fmt.Print(1)
+
+	if x != 5 {
+		fmt.Print(2)
+	}
+	fmt.Print(3)
+}
