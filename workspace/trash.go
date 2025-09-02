f3cf1d3cc4ff3dcb99b11f03b3eecc79efcaa7e9
Safin <ilnursafin35@gmail.com>
2025-09-02 05:03:13 +0500

    2.09.2025

--- workspace/trash.go
+++ workspace/trash.go
@@ -4,23 +4,21 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	new_scanner := bufio.NewScanner(os.Stdin)
-	for new_scanner.Scan() {
-		name := new_scanner.Text()
-		fmt.Print(name)
-	}
+	reader := bufio.NewReader(os.Stdin)
+	delimiter, _ := reader.ReadString('\n')
+	delimiter = strings.TrimSpace(delimiter)
+	s1, _ := reader.ReadString('\n')
+	s1 = strings.TrimSpace(s1)
+	s2, _ := reader.ReadString('\n')
+	s2 = strings.TrimSpace(s2)
+	s3, _ := reader.ReadString('\n')
+	s3 = strings.TrimSpace(s3)
+	fmt.Print(s1, delimiter, s2, delimiter, s3)
 }
-
-// func main() {
-// 	new_scanner := bufio.NewScanner(os.Stdin)
-// 	for new_scanner.Scan() {
-// 		name := new_scanner.Text()
-// 		fmt.Print(name)
-// 	}
-// }
 
 // func main() {
 // 	new_scanner := bufio.NewScanner(os.Stdin)
