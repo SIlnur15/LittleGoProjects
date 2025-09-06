815fd006b8f969a3c6c42b04e161d79d922d201d
Safin <ilnursafin35@gmail.com>
2025-09-06 23:58:20 +0500

    6.9.25

--- workspace/trash.go
+++ workspace/trash.go
@@ -1,15 +1,10 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	var input int
 	fmt.Scan(&input)
-	if (input%4 == 0 && input%100 != 0) || input%400 == 0 {
-		fmt.Println("YES")
-	} else {
-		fmt.Println("NO")
-	}
+	fmt.Printf("%T\n", input/2)
+	fmt.Println(input / 2)
 }
