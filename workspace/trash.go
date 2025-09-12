145f4f2348b16594b286ea815c2f63ec85053690
Safin <ilnursafin35@gmail.com>
2025-09-13 00:52:41 +0500

    13.09.25

--- workspace/trash.go
+++ workspace/trash.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	k := "abac"
-	fmt.Println(k)
+	var input string
+	fmt.Scan(&input)
+	for _, letter := range input {
+		if strings.Count(input, string(letter)) == 2 {
+			fmt.Println(string(letter))
+			break
+		}
+	}
 }
