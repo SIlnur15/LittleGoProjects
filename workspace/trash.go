6468fccba0894f494eb2527e5d34b8fe7f0f7d45
Safin <ilnursafin35@gmail.com>
2025-09-17 23:31:59 +0500

    17.09.25

--- workspace/trash.go
+++ workspace/trash.go
@@ -1,14 +1,30 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
+	"math"
 )
 
 func main() {
-	input := bufio.NewScanner(os.Stdin)
-	input.Scan()
-	output := input.Text()
-	fmt.Println(output)
+	var evens, not_evens []int
+	var val int
+
+	for i := 0; i < 15; i++ {
+		fmt.Scan(&val)
+		if val%2 == 0 {
+			evens = append(evens, val)
+		} else {
+			not_evens = append(not_evens, val)
+		}
+	}
+	length := int(math.Max(float64(len(evens)), float64(len(not_evens))))
+
+	for i := 0; i < length; i++ {
+		if i < len(evens) {
+			fmt.Printf("%d ", evens[i])
+		}
+		if i < len(not_evens) {
+			fmt.Printf("%d ", not_evens[i])
+		}
+	}
 }
