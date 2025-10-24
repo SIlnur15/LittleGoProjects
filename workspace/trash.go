293bc0bfa2065d5d843a36206d87e1591d9df168
Safin <ilnursafin35@gmail.com>
2025-10-25 03:28:16 +0500

    it's Friday

--- workspace/trash.go
+++ workspace/trash.go
@@ -1,38 +1,13 @@
 package main
 
-import (
-    "fmt"
-    "sort"
-)
+import "fmt"
 
-// Функция для проверки, являются ли две строки анаграммами
-func isAnagram(s1, s2 string) bool {
-    // Если длины строк не совпадают, они не могут быть анаграммами
-    if len(s1) != len(s2) {
-        return false
-    }
-    
-    // Преобразуем строки в срезы рун (для корректной работы с Unicode)
-    r1 := []rune(s1)
-    r2 := []rune(s2)
-    
-    // Сортируем оба среза
-    sort.Slice(r1, func(i, j int) bool { return r1[i] < r1[j] })
-    sort.Slice(r2, func(i, j int) bool { return r2[i] < r2[j] })
-    
-    // Сравниваем отсортированные срезы
-    return string(r1) == string(r2)
+func exper(cat ...int) {
+    fmt.Println(len(cat))
 }
 
 func main() {
-	// не изменяйте функцию main!!
-
-	var word1, word2 string
-	fmt.Scan(&word1, &word2)
-
-	if isAnagram(word1, word2) {
-		fmt.Println("Да")
-	} else {
-		fmt.Println("Нет")
-	}
+    kek := []int{5,15,25}
+    exper(kek...)   // 3
 }
+
