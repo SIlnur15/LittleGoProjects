0595405cb5a9b85473a41014efc55ede7cd8b02a
Safin <ilnursafin35@gmail.com>
2025-10-26 04:49:47 +0500

    saturday

--- workspace/prose/prose_test/go.mod
+++
@@ -1,3 +0,0 @@
-module prose_test
-
-go 1.18
--- workspace/trash.go
+++ workspace/trash.go
@@ -1,13 +1,28 @@
 package main
 
-import "fmt"
-
-func exper(cat ...int) {
-    fmt.Println(len(cat))
-}
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-    kek := []int{5,15,25}
-    exper(kek...)   // 3
-}
+	// Создаём файл (если он существует, он будет перезаписан)
+	file, err := os.Create("output.txt")
+	if err != nil {
+		fmt.Println("Ошибка создания файла:", err)
+		return
+	}
+	defer file.Close() // Закрываем файл после завершения работы
 
+	// Текст для записи
+	text := "Привет, мир! Это текст, записанный в файл.\n"
+
+	// Записываем текст в файл
+	_, err = file.WriteString(text)
+	if err != nil {
+		fmt.Println("Ошибка записи в файл:", err)
+		return
+	}
+
+	fmt.Println("Данные успешно записаны в файл.")
+}
