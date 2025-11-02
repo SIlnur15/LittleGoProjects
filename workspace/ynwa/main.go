57520ec1f1f500441139f2120cbdcbe336a14a2a
Safin <ilnursafin35@gmail.com>
2025-11-03 00:14:35 +0500

    the third of november

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -2,19 +2,11 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
-func safeAdd32(a, b int32) (int32, bool) {
-	if b > 0 && a > math.MaxInt32-b {
-		return 0, false // Переполнение
-	}
-	if b < 0 && a < math.MinInt32-b {
-		return 0, false // Потеря значимости
-	}
-	return a + b, true
-}
-
 func main() {
-	fmt.Println(safeAdd32(5, 15))
+	s := "Go123"
+	for _, v := range s {
+		fmt.Print(v, " ")
+	}
 }
