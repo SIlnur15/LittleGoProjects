67443e1cc01b400303e80e816f309cbf25b35502
Safin <ilnursafin35@gmail.com>
2025-11-04 00:13:01 +0500

    4.11.25

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -1,12 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	s := "Go123"
-	for _, v := range s {
-		fmt.Print(v, " ")
-	}
+	fmt.Println(true) // true
+	true := 10
+	fmt.Println(true) // 10
 }
