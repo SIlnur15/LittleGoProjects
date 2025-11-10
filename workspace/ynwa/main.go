3defd29d626304cf2ab3fe43e2769bf93f1fd85f
Safin <ilnursafin35@gmail.com>
2025-11-10 17:49:19 +0500

    binary

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -1,5 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
-	print(525)
+	num := 123.456789
+	fmt.Printf("%%f:     '%f'\n", num)    // стандарт
+	fmt.Printf("%%9f:    '%9f'\n", num)   // ширина 9
+	fmt.Printf("%%.2f:   '%.2f'\n", num)  // точность 2
+	fmt.Printf("%%9.2f:  '%9.2f'\n", num) // ширина 9, точность 2
+	fmt.Printf("%%9.f:   '%9.f'\n", num)  // ширина 9, точность 0
+	fmt.Printf("%%09.f:  '%09.f'\n", num) // нули, ширина 9, точность 0
 }
