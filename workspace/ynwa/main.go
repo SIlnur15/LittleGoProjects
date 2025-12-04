486d2eacc1770473848aa743009f6cfeb3715639
Safin <ilnursafin35@gmail.com>
2025-12-04 23:56:02 +0500

    Yes I'm yet here

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -1,15 +1,20 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
+import "fmt"
 
 func main() {
-	fmt.Print("please enter your name: ")
-	littleScanner := bufio.NewScanner(os.Stdin)
-	littleScanner.Scan()
-	name := littleScanner.Text()
-	fmt.Printf("So, nice to meet you, %s!", name)
+	var num int
+	fmt.Scan(&num)
+	switch num {
+	case 2:
+		fmt.Println("Неудовлетворительно")
+	case 3:
+		fmt.Println("Удовлетворительно")
+	case 4:
+		fmt.Println("Хорошо")
+	case 5:
+		fmt.Println("Отлично")
+	default:
+		fmt.Println("Некорректное значение!")
+	}
 }
