47299f58ebeac0076659d3119cbe104d39545a1e
Safin <ilnursafin35@gmail.com>
2025-12-12 23:49:45 +0500

    hello worls

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -3,13 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	var num, sum int
-	for {
-		fmt.Scan(&num)
-		sum += num
-		if num == 0 {
-			break
-		}
-	}
-	fmt.Println(sum)
+	fmt.Println("hello world")
 }
