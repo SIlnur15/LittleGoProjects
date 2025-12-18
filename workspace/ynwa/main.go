c837dba53a8188461229937c752cc9676d37d4a1
Safin <ilnursafin35@gmail.com>
2025-12-18 19:51:07 +0500

    second working day

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -3,12 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	var n int
-	fmt.Scan(&n)
-	for i := 1; i <= n; i++ {
-		for j := 1; j <= i; j++ {
-			fmt.Printf("%d ", j)
-		}
-		fmt.Println()
-	}
+	arr := [7]int{4, 2, 9, 5, 6, 7}
+	var first, second int
+	fmt.Scan(&first, &second)
+	fmt.Println(arr[first], arr[second])
 }
