74fc52a2d302a9210683f9bcdab75ca6929dd875
Safin <ilnursafin35@gmail.com>
2025-12-22 17:03:43 +0500

    slices

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -1,19 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
 	var (
-		n   int
-		inp int
-		arr []int
+		n          int
+		arr1, arr2 []int
 	)
-	fmt.Scan(&n)
+	scanner := bufio.NewScanner(os.Stdin)
+
 	for i := 0; i < n; i++ {
-		fmt.Scan(&inp)
-		if inp%2 == 0 {
-			arr = append(arr, inp)
-		}
+		scanner.Scan()
+		input := scanner.Text()
+		input_int, _ := strconv.Atoi(input)
+		arr1[i] = input_int
 	}
-	fmt.Println(arr)
+	for i := 0; i < n; i++ {
+		scanner.Scan()
+		input := scanner.Text()
+		input_int, _ := strconv.Atoi(input)
+		arr2[i] = input_int
+	}
+	fmt.Println(arr1, arr2)
 }
