eb98c207125275fee2ad6e6952fb7d33ab739435
Safin <ilnursafin35@gmail.com>
2025-12-24 03:17:47 +0500

    map

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -1,30 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
 )
 
 func main() {
-	var (
-		n          int
-		arr1, arr2 []int
-	)
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for i := 0; i < n; i++ {
-		scanner.Scan()
-		input := scanner.Text()
-		input_int, _ := strconv.Atoi(input)
-		arr1[i] = input_int
-	}
-	for i := 0; i < n; i++ {
-		scanner.Scan()
-		input := scanner.Text()
-		input_int, _ := strconv.Atoi(input)
-		arr2[i] = input_int
-	}
-	fmt.Println(arr1, arr2)
+	var m map[string]int
+	m["key"] = 15
+	fmt.Println(m) // panic
 }
