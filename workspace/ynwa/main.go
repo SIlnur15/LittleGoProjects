d438c1bf8853e337e90073d7d3dd786f7c7e6b42
Safin <ilnursafin35@gmail.com>
2025-12-25 00:05:27 +0500

    a task from a interview

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -1,11 +1,36 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+func lengthOfLongestSubstring(s string) int {
+	charIndex := make(map[rune]int) // карта для хранения индексов каждого символа
+	maxLen := 0
+	start := 0 // начальный индекс текущей подстроки без дубликатов
+	count := 1
+
+	for i, ch := range s {
+		fmt.Println(count)
+		fmt.Println(charIndex)
+		if lastIdx, ok := charIndex[ch]; ok && lastIdx >= start {
+			// если символ уже есть в мапе
+			fmt.Println(lastIdx, ok)
+			// если символ повторяется, то перемещаемся на следующую позицию после последнего появления
+			start = lastIdx + 1
+		}
+		charIndex[ch] = i
+		if i-start+1 > maxLen {
+			maxLen = i - start + 1
+		}
+		fmt.Println(charIndex)
+		count += 1
+		fmt.Println(maxLen)
+		fmt.Println("========")
+	}
+
+	return maxLen
+}
 
 func main() {
-	var m map[string]int
-	m["key"] = 15
-	fmt.Println(m) // panic
+	s := "abcabcbb"
+	fmt.Println(lengthOfLongestSubstring(s)) // Вывод: 3
 }
