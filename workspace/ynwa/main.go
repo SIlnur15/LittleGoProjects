20a55d74019140832e830ac552fde2365e802c3f
Safin <ilnursafin35@gmail.com>
2025-12-26 00:07:58 +0500

    dcts

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -2,35 +2,13 @@ package main
 
 import "fmt"
 
-func lengthOfLongestSubstring(s string) int {
-	charIndex := make(map[rune]int) // карта для хранения индексов каждого символа
-	maxLen := 0
-	start := 0 // начальный индекс текущей подстроки без дубликатов
-	count := 1
-
-	for i, ch := range s {
-		fmt.Println(count)
-		fmt.Println(charIndex)
-		if lastIdx, ok := charIndex[ch]; ok && lastIdx >= start {
-			// если символ уже есть в мапе
-			fmt.Println(lastIdx, ok)
-			// если символ повторяется, то перемещаемся на следующую позицию после последнего появления
-			start = lastIdx + 1
-		}
-		charIndex[ch] = i
-		if i-start+1 > maxLen {
-			maxLen = i - start + 1
-		}
-		fmt.Println(charIndex)
-		count += 1
-		fmt.Println(maxLen)
-		fmt.Println("========")
-	}
-
-	return maxLen
-}
-
 func main() {
-	s := "abcabcbb"
-	fmt.Println(lengthOfLongestSubstring(s)) // Вывод: 3
+	s1 := make([]int, 3, 6)
+	s2 := s1[1:3]
+	fmt.Println(s1, s2)
+	s1 = append(s1, 1)
+	fmt.Println(s1)
+	s2 = append(s2, 2)
+	fmt.Println(s1, s2)
+	fmt.Println(&s1[3], &s2[2])
 }
