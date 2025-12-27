566ba0ce7accd5e6ac4d5d353bb9c6eb7e17c50f
Safin <ilnursafin35@gmail.com>
2025-12-28 01:03:53 +0500

    funcs

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -1,39 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"slices"
-)
-
-func union(a, b []int) []int {
-	for i := 0; i < len(b); i++ {
-		a = append(a, b[i])
-	}
-	slices.Sort(a)
-	slices.Compact(a)
-	b = make([]int, 0)
-	for i := 0; i < len(a); i++ {
-		if a[i] != 0 {
-			b = append(b, a[i])
-		}
-	}
-	return b
-}
+import "fmt"
 
 func main() {
-	var (
-		a, b, value int
-	)
-	fmt.Scan(&a, &b)
-	arr1 := make([]int, a)
-	arr2 := make([]int, b)
-	for i := 0; i < a; i++ {
-		fmt.Scan(&value)
-		arr1[i] = value
+	a := func(name string) {
+		fmt.Println("hello", name)
 	}
-	for i := 0; i < b; i++ {
-		fmt.Scan(&value)
-		arr2[i] = value
-	}
-	fmt.Println(union(arr1, arr2))
+	a("Roma")
+	a("Sasha")
 }
