e47143d9a606bb0c7f33ea0fffbd926feb88f79c
Safin <ilnursafin35@gmail.com>
2025-12-31 03:09:39 +0500

    anonim function

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -1,19 +1,13 @@
 package main
 
-import "fmt"
-
-// Callback-функция как параметр
-func processNumbers(numbers []int, callback func(int)) {
-	for _, num := range numbers {
-		callback(num) // Вызов callback для каждого числа
-	}
-}
-
 func main() {
-	numbers := []int{1, 2, 3, 4, 5}
-
-	// Передаем анонимную функцию как callback
-	processNumbers(numbers, func(n int) {
-		fmt.Printf("Число: %d\n", n)
-	})
+	filteredStrings := func(slc []string, callback func(string) bool) []string {
+		result := make([]string, 0)
+		for _, elem := range slc {
+			if callback(elem) {
+				ret = append(ret, elem)
+			}
+		}
+		return result
+	}
 }
