e0763d5baf4d92a95aa0dc661bdd514f526f4536
Safin <ilnursafin35@gmail.com>
2026-01-03 00:46:16 +0500

    3rd January

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -2,13 +2,11 @@ package main
 
 import "fmt"
 
-func dubleMaker(numb *int) {
-	*numb = *numb * 2
-}
-
 func main() {
-	a := 10
-	fmt.Println(a)
-	dubleMaker(&a)
-	fmt.Println(a)
+	a := 100
+	ptr := &a
+	ptr2 := new(int)
+	ptr2 = ptr
+	*ptr = 200
+	fmt.Println(*ptr, *ptr2)
 }
