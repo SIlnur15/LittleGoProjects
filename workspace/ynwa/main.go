9eaa4b8cb9879efe3b0e3eba1207edb158b375cc
Safin <ilnursafin35@gmail.com>
2026-01-05 01:15:15 +0500

    structures

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -2,17 +2,12 @@ package main
 
 import "fmt"
 
-func changePointer(pp **int, newValue int) {
-	*pp = &newValue      // разыменовываем pp и присваиваем ему адрес переменной newValue
-	fmt.Println(pp, *pp) // 0xc000070030 0xc000010100 - оба вывода - адреса
+type Contact struct {
+	name string
+	age  int
 }
 
 func main() {
-	x := 10
-	ptr := &x // ptr — указатель на x (т.е. хранит адрес переменной x)
-
-	y := 20
-	changePointer(&ptr, y) // в функцию передаём: адрес указателя ptr и значение 20
-
-	fmt.Println(*ptr) // 20
+	x := Contact{"Klopp", 15}
+	fmt.Printf("%#v\n", x)
 }
