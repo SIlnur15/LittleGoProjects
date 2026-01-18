bf3a91a058317b65b862345f91595ba035e6293b
Safin <ilnursafin35@gmail.com>
2026-01-18 20:49:51 +0500

    errors day second

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -1,5 +1,28 @@
 package main
 
-type error interface {
-	Error() string
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	err := doSomething()
+	if err != nil {
+		fmt.Printf("Ошибка: %v\n", err) // Выведет обёрнутую ошибку
+
+		// Проверяем, содержится ли исходная ошибка в цепочке
+		if errors.Is(err, ErrOriginal) {
+			fmt.Println("Да, это исходная ошибка!")
+		}
+	}
+}
+
+var ErrOriginal = errors.New("исходная ошибка")
+
+func doSomething() error {
+	// Имитируем ошибку
+	err := ErrOriginal
+
+	// Оборачиваем ошибку с дополнительным контекстом
+	return fmt.Errorf("не удалось выполнить действие: %w", err)
 }
