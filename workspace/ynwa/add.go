02247ef9d905303c3133af7f4e866d7689a74402
Safin <ilnursafin35@gmail.com>
2026-02-10 22:44:11 +0500

    10th February second

--- workspace/ynwa/add.go
+++ workspace/ynwa/add.go
@@ -1,5 +1,18 @@
 package main
 
-func add(a, b int) int {
-	return a + b
+import "fmt"
+
+type Reader interface {
+	Read(p []byte) (n int, err error)
+}
+
+type LoggingReader struct {
+	r Reader
+}
+
+func (lr *LoggingReader) Read(p []byte) (int, error) {
+	fmt.Println("Начинаем читать данные")
+	n, err := lr.r.Read(p)
+	fmt.Println("Прочитано байт:", n)
+	return n, err
 }
--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -1,22 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
-type account struct {
-	balance float32
+func max(arr ...int) int {
+	mx := 0
+	for _, elem := range arr {
+		if elem > mx {
+			mx = elem
+		}
+	}
+	return mx
 }
 
 func main() {
-	accounts := []account{
-		{balance: 100},
-		{balance: 200},
-		{balance: 300},
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
+	kaput := strings.Fields(input)
+	kaputInt := make([]int, len(kaput))
+	for idx, _ := range kaput {
+		kaputIntElem, _ := strconv.Atoi(kaput[idx])
+		kaputInt[idx] = kaputIntElem
 	}
-	for _, a := range accounts {
-		a.balance += 1000
-	}
-	for idx, _ := range accounts {
-		accounts[idx].balance += 1000
-	}
-	fmt.Println(accounts)
+	fmt.Println(max(kaputInt...))
 }
