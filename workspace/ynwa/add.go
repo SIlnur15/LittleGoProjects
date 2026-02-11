93a4041af0105becce945cf4377358935c4aa323
Safin <ilnursafin35@gmail.com>
2026-02-12 00:41:30 +0500

    12th February

--- workspace/ynwa/add.go
+++ workspace/ynwa/add.go
@@ -1,18 +1,8 @@
 package main
 
-import "fmt"
-
-type Reader interface {
-	Read(p []byte) (n int, err error)
-}
-
-type LoggingReader struct {
-	r Reader
-}
-
-func (lr *LoggingReader) Read(p []byte) (int, error) {
-	fmt.Println("Начинаем читать данные")
-	n, err := lr.r.Read(p)
-	fmt.Println("Прочитано байт:", n)
-	return n, err
+mapIntegers := func(nums []int, callback func(int) int) []int {
+    for idx,elem := range nums {
+		nums[idx] = callback(elem)
+	}
+	return nums
 }
--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -2,14 +2,48 @@ package main
 
 import "fmt"
 
-type Callback func(string)
+type Parent struct {
+	Name     string
+	Children []Child
+}
 
-func notify(cb Callback, message string) {
-	cb(message)
+type Child struct {
+	Name string
+	Age  int
+}
+
+func CopyParent(p *Parent) Parent {
+	if p == nil {
+		return Parent{}
+	}
+	slice2 := make([]Child, len(p.Children))
+	copy(slice2, p.Children) // теперь у slice2 другая ссылка
+	return Parent{Name: p.Name, Children: slice2}
 }
 
 func main() {
-	notify(func(msg string) {
-		fmt.Println("statement:", msg)
-	}, "hello")
+	p := &Parent{
+		Name: "Harry",
+		Children: []Child{
+			{
+				Name: "Andy",
+				Age:  18,
+			},
+		},
+	}
+
+	cp := CopyParent(p)
+
+	// при мутациях в копии "cp"
+	// изначальная структура "p" не изменяется
+	cp.Children[0] = Child{
+		Name: "Gosha",
+		Age:  30,
+	}
+
+	fmt.Println(p.Children)  // -> [{Andy 18}]
+	fmt.Println(cp.Children) // -> [{Gosha 30}]
+
+	cp2 := CopyParent(nil) // -> Parent{}
+	fmt.Println(cp2)
 }
