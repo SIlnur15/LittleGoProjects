d476f0dc0113dd9490986242a4158c4dba8dab17
Safin <ilnursafin35@gmail.com>
2026-02-13 00:09:01 +0500

    13th Friday

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -1,49 +1,20 @@
 package main
 
-import "fmt"
-
-type Parent struct {
-	Name     string
-	Children []Child
-}
-
-type Child struct {
-	Name string
-	Age  int
-}
-
-func CopyParent(p *Parent) Parent {
-	if p == nil {
-		return Parent{}
-	}
-	slice2 := make([]Child, len(p.Children))
-	copy(slice2, p.Children) // теперь у slice2 другая ссылка
-	return Parent{Name: p.Name, Children: slice2}
-}
+import (
+	"fmt"
+	"unicode"
+)
 
 func main() {
-	p := &Parent{
-		Name: "Harry",
-		Children: []Child{
-			{
-				Name: "Andy",
-				Age:  18,
-			},
-		},
+	var (
+		input  string
+		export int
+	)
+	fmt.Scan(&input)
+	for _, elem := range input {
+		if unicode.IsDigit(elem) {
+			export += 1
+		}
 	}
-
-	cp := CopyParent(p)
-
-	// при мутациях в копии "cp"
-	// изначальная структура "p" не изменяется
-	cp.Children[0] = Child{
-		Name: "Gosha",
-		Age:  30,
-	}
-
-	fmt.Println(p.Children)  // -> [{Andy 18}]
-	fmt.Println(cp.Children) // -> [{Gosha 30}]
-
-	cp2 := CopyParent(nil) // -> Parent{}
-	fmt.Println(cp2)
+	fmt.Printf("Количество цифр в строке: %d\n", export)
 }
