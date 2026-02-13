1f41f799959a2ce8a96be1c0ea18e5986af0b39f
Safin <ilnursafin35@gmail.com>
2026-02-14 00:06:42 +0500

    14th February

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -1,20 +1,45 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"unicode"
+	"os"
+	"strings"
+	"unicode/utf8"
 )
 
+func mapper(w rune) rune {
+	switch w {
+	case '!', '-', ',', '?', '.', ';', ':':
+		return 0 // далее в теле программы нужно будет избавляться от пустых рун
+		// потому что, как оказалось, их хоть и не видно, но они есть
+	}
+	return w
+}
+
 func main() {
-	var (
-		input  string
-		export int
-	)
-	fmt.Scan(&input)
-	for _, elem := range input {
-		if unicode.IsDigit(elem) {
-			export += 1
+	// Считывание всей строки
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	str := sc.Text()
+
+	export := strings.Map(mapper, str)
+	exportRunes := []rune(export)
+	sample := []rune{}
+	for idx, elem := range exportRunes {
+		if elem != 0 {
+			sample = append(sample, exportRunes[idx])
 		}
 	}
-	fmt.Printf("Количество цифр в строке: %d\n", export)
+	word := string(sample)        // получаю строку из среза рун
+	words := strings.Fields(word) // Разбиение строки на слова по пробелам
+
+	// Поиск самого длинного слова
+	longestWord := ""
+	for _, word := range words {
+		if utf8.RuneCountInString(word) > utf8.RuneCountInString(longestWord) {
+			longestWord = word
+		}
+	}
+	fmt.Println(longestWord)
 }
