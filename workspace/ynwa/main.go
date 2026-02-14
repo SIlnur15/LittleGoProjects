aba93b80490a14100762555fbdab038a3f4cb36a
Safin <ilnursafin35@gmail.com>
2026-02-14 21:17:15 +0500

    14th February second commit

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -1,45 +1,19 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-	"unicode/utf8"
+	"log"
+	"net/http"
 )
 
-func mapper(w rune) rune {
-	switch w {
-	case '!', '-', ',', '?', '.', ';', ':':
-		return 0 // далее в теле программы нужно будет избавляться от пустых рун
-		// потому что, как оказалось, их хоть и не видно, но они есть
+func viewHandler(writer http.ResponseWriter, request *http.Request) {
+	message := []byte("Hello, web!")
+	_, err := writer.Write(message)
+	if err != nil {
+		log.Fatal(err)
 	}
-	return w
 }
-
 func main() {
-	// Считывание всей строки
-	sc := bufio.NewScanner(os.Stdin)
-	sc.Scan()
-	str := sc.Text()
-
-	export := strings.Map(mapper, str)
-	exportRunes := []rune(export)
-	sample := []rune{}
-	for idx, elem := range exportRunes {
-		if elem != 0 {
-			sample = append(sample, exportRunes[idx])
-		}
-	}
-	word := string(sample)        // получаю строку из среза рун
-	words := strings.Fields(word) // Разбиение строки на слова по пробелам
-
-	// Поиск самого длинного слова
-	longestWord := ""
-	for _, word := range words {
-		if utf8.RuneCountInString(word) > utf8.RuneCountInString(longestWord) {
-			longestWord = word
-		}
-	}
-	fmt.Println(longestWord)
+	http.HandleFunc("/hello", viewHandler)
+	err := http.ListenAndServe("localhost:8080", nil)
+	log.Fatal(err)
 }
