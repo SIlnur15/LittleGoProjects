025e80e68d6dc844c3a6025cf7b0e6bb4174d16f
Safin <ilnursafin35@gmail.com>
2026-02-15 22:17:58 +0500

    15th february

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
-	"log"
-	"net/http"
+	"fmt"
+	"os"
 )
 
-func viewHandler(writer http.ResponseWriter, request *http.Request) {
-	message := []byte("Hello, web!")
-	_, err := writer.Write(message)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
 func main() {
-	http.HandleFunc("/hello", viewHandler)
-	err := http.ListenAndServe("localhost:8080", nil)
-	log.Fatal(err)
+	entries, err := os.ReadDir(".")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	for _, entry := range entries {
+		info, _ := entry.Info() // Получаем os.FileInfo (если нужно)
+		fmt.Printf(
+			"Name: %-10s | IsDir: %-5t | Size: %d\n",
+			entry.Name(),
+			entry.IsDir(),
+			info.Size(),
+		)
+	}
 }
