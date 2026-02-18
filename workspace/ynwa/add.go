8a08acbf1cb9470d42af3624213b578265b9c89a
Safin <ilnursafin35@gmail.com>
2026-02-19 00:14:48 +0500

    February 19th

--- workspace/ynwa/add.go
+++ workspace/ynwa/add.go
@@ -1,8 +1,27 @@
 package main
 
-mapIntegers := func(nums []int, callback func(int) int) []int {
-    for idx,elem := range nums {
-		nums[idx] = callback(elem)
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+	content, err := os.ReadFile("stepik_io_1.text")
+	if err != nil {
+		fmt.Errorf("there is a problem")
+		return
 	}
-	return nums
+	input := string(content)
+	fmt.Println(input)
+	re := regexp.MustCompile(`\+?\d+`)
+	matches := re.FindAllString(input, -1)
+
+	sum := 0
+	for _, match := range matches {
+		num, _ := strconv.Atoi(match)
+		sum += num
+	}
+	fmt.Println(sum)
 }
--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -1,21 +1,25 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 )
 
 func main() {
-	// Создаем новый сканер из стандартного ввода
-	scanner := bufio.NewScanner(os.Stdin)
-
-	// Устанавливаем разделитель
-	scanner.Split(bufio.ScanWords)
-
-	// Читаем слова из ввода и выводим их на экран
-	for scanner.Scan() {
-		word := scanner.Text()
-		fmt.Println(word)
+	content, err := os.ReadFile("stepik_io_1.text")
+	if err != nil {
+		fmt.Printf("%v", err)
+		return
 	}
+	input := string(content)
+	re := regexp.MustCompile(`\d+`)
+	matches := re.FindAllString(input, -1)
+	sum := 0
+	for _, match := range matches {
+		num, _ := strconv.Atoi(match)
+		sum += num
+	}
+	fmt.Println(sum)
 }
