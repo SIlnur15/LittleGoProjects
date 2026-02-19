9dfc418dfbc80313e703484782c49a16d013cda4
Safin <ilnursafin35@gmail.com>
2026-02-20 03:00:16 +0500

    February 20th

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -2,24 +2,10 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"regexp"
-	"strconv"
+	"time"
 )
 
 func main() {
-	content, err := os.ReadFile("stepik_io_1.text")
-	if err != nil {
-		fmt.Printf("%v", err)
-		return
-	}
-	input := string(content)
-	re := regexp.MustCompile(`\d+`)
-	matches := re.FindAllString(input, -1)
-	sum := 0
-	for _, match := range matches {
-		num, _ := strconv.Atoi(match)
-		sum += num
-	}
-	fmt.Println(sum)
+	d := 5*time.Minute + 10*time.Hour // 10 hours 5 минут
+	fmt.Println(d)                    // 10h5m0s
 }
