ecc0b8b8bbb7bb91ffd6efa06bed5ff83ca7705f
Safin <ilnursafin35@gmail.com>
2026-02-27 03:46:09 +0500

    February 27th

--- workspace/ynwa/add.go
+++ workspace/ynwa/add.go
@@ -3,10 +3,42 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 )
 
 func main() {
-	jsonData := []byte(`{"name":"John","age":30,"city":"New York"`)
-	isValid := json.Valid(jsonData)
-	fmt.Println(isValid)
+	var (
+		output map[string]interface{}
+		summa  float64
+		lenArr float64
+	)
+
+	file, err := os.Open("stepik4-4-2.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&output)
+	if err != nil {
+		fmt.Println("2. trouble")
+	}
+	if students, ok := output["students"].([]interface{}); ok {
+		for _, student := range students {
+			if studentStudent, ok := student.(map[string]interface{}); ok {
+				if person, ok := studentStudent["Rating"].(interface{}); ok {
+					if arr, ok := person.([]interface{}); ok {
+						for _, elem := range arr {
+							if elemElem, ok := elem.(float64); ok {
+								summa += elemElem
+							}
+							lenArr += 1
+						}
+					}
+				}
+			}
+		}
+		fmt.Printf("%.2f\n", summa/lenArr)
+	}
 }
--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -7,23 +7,38 @@ import (
 )
 
 func main() {
-	var output map[string]interface{}
+	var (
+		output map[string]interface{}
+		summa  float64
+		lenArr float64
+	)
 
-	file, err := os.Open("stepik4-4.json")
+	file, err := os.Open("stepik4-4-2.json")
 	if err != nil {
-		fmt.Println("trouble")
+		fmt.Println(err)
 	}
 	defer file.Close()
 
 	decoder := json.NewDecoder(file)
 	err = decoder.Decode(&output)
-	if people, ok := output["people"].([]interface{}); ok {
-		for _, person := range people {
-			if gender, ok := person.(map[string]interface{}); ok {
-				if gender["gender"] == "Female" {
-					fmt.Printf("%s %s\n", gender["first_name"], gender["last_name"])
+	if err != nil {
+		fmt.Println("2. trouble")
+	}
+	if students, ok := output["students"].([]interface{}); ok {
+		for _, student := range students {
+			if studentStudent, ok := student.(map[string]interface{}); ok {
+				if person, ok := studentStudent["Rating"].(interface{}); ok {
+					if arr, ok := person.([]interface{}); ok {
+						for _, elem := range arr {
+							if elemElem, ok := elem.(float64); ok {
+								summa += elemElem
+							}
+							lenArr += 1
+						}
+					}
 				}
 			}
 		}
+		fmt.Printf("%.2f\n", summa/lenArr)
 	}
 }
