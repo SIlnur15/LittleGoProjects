04a61319cfd068f678308effd751b4f92c1396cc
Safin <ilnursafin35@gmail.com>
2026-03-01 16:31:06 +0500

    let's net/http

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -1,29 +1,9 @@
 package main
 
-import (
-	"encoding/json"
-	"os"
-)
+import "net/http"
 
-// Пример структуры
-type Person struct {
-	Name string `json:"name"`
-	Age  int    `json:"age"`
-}
-
-// Чтение структуры из JSON файла
-func loadJSON(filename string, data interface{}) error {
-	file, err := os.Open(filename)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	decoder := json.NewDecoder(file)
-
-	err = decoder.Decode(data)
-	if err != nil {
-		return err
-	}
-	return nil
+type ResponseWriter interface {
+	Header() http.Header
+	Write([]byte) (int, error)
+	WriteHeader(statusCode int)
 }
