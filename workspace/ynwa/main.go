37908f14ab359c561fe904d0a27021f49a5198ba
Safin <ilnursafin35@gmail.com>
2026-03-16 23:17:28 +0500

    ServeMux

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -1,21 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
-
-type myHandler struct{}
-
-func (h *myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, world!"))
-}
-
-func main() {
-	http.Handle("/", &myHandler{})
-	err := http.ListenAndServe("localhost:8080", nil)
-	if err != nil {
-		fmt.Println("ListenAndServe: ", err)
-		return
-	}
+type ServeMux struct {
+	// содержит поля для хранения маршрутов
 }
