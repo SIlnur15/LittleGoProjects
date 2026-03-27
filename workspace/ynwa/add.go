113998b2f6b38b1232db533c31c65276be98e85b
Safin <ilnursafin35@gmail.com>
2026-03-28 01:59:06 +0500

    March 28th

--- workspace/ynwa/add.go
+++ workspace/ynwa/add.go
@@ -1,28 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
-	"time"
 )
 
 func main() {
-	client := &http.Client{
-		Timeout: time.Second * 5, // Wait no more than 5 seconds for an answer
-
-	}
-
-	resp, err := client.Get("https://api.chucknorris.io/jokes/random")
+	sum := 0
+	req, err := http.Get("https://open.er-api.com/v6/latest/USD")
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
-	defer resp.Body.Close()
+	defer req.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
+	var rts map[string]interface{}
+
+	decoder := json.NewDecoder(req.Body)
+	err = decoder.Decode(&rts)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
-
-	fmt.Println(string(body))
+	if Rates, ok := rts["rates"].(map[string]interface{}); ok {
+		for valute := range Rates {
+			if string(valute[0]) == "T" {
+				sum += 1
+			}
+		}
+	}
+	fmt.Println(sum)
 }
