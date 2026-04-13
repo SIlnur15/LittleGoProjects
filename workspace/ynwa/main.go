c28eff10686e5cdfc852a5d42d8724188c869ece
Safin <ilnursafin35@gmail.com>
2026-04-13 19:58:41 +0500

    context

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -1,34 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
+import "time"
 
-func helloEnHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, world!"))
-}
-
-func helloRuHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Привет, мир!"))
-}
-
-func helloEsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hola, Mundo!"))
-}
-
-func helloJpHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("「こんにちは世界」"))
-}
-
-func main() {
-	http.HandleFunc("/en", helloEnHandler)
-	http.HandleFunc("/ru", helloRuHandler)
-	http.HandleFunc("/es", helloEsHandler)
-	http.HandleFunc("/jp", helloJpHandler)
-	err := http.ListenAndServe("localhost:8080", nil)
-	if err != nil {
-		fmt.Println("ListenAndServe: ", err)
-		return
-	}
+type Context interface {
+	Deadline() (deadline time.Time, ok bool)
+	Done() <-chan struct{}
+	Err() error
+	Value(key any) any
 }
