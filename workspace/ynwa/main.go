bfb79c0b12feb6b851611a084ff2b986c885bf35
Safin <ilnursafin35@gmail.com>
2026-04-16 03:22:55 +0500

    April 16th

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -1,12 +1,31 @@
 package main
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	type Context interface {
-		Deadline() (deadline time.Time, ok bool)
-		Done() <-chan struct{}
-		Err() error
-		Value(key any) any
+	resp, err := http.Get("http://api.zippopotam.us/us/90210")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	var cont map[string]interface{}
+
+	dec := json.NewDecoder(resp.Body)
+	err = dec.Decode(&cont)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if places, ok := cont["places"].([]interface{}); ok {
+		if sample, ok := places[0].(map[string]interface{}); ok {
+			fmt.Println(sample["place name"]) // Beverly Hills
+		}
 	}
 }
