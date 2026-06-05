e9517d2cbeec1270af2685f7933594d59cef893e
Safin <ilnursafin35@gmail.com>
2026-06-06 03:10:11 +0500

    6th June

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -8,10 +8,16 @@ type myType struct {
 }
 
 func (m myType) get() {
-	fmt.Printf("hello, I'm %s\n", m.name)
+	fmt.Printf("hello, I'm %s, I'm %d\n", m.name, m.age)
+}
+
+func (m *myType) set() {
+	m.age = m.age + 5
 }
 
 func main() {
 	a := myType{name: "Axel", age: 25}
+	a.get()
+	a.set()
 	a.get()
 }
