6076503ab4363b92eccd1f794527d5afba6f7934
Safin <ilnursafin35@gmail.com>
2026-06-07 19:20:11 +0500

    funcs

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -2,22 +2,10 @@ package main
 
 import "fmt"
 
-type myType struct {
-	name string
-	age  int
-}
-
-func (m myType) get() {
-	fmt.Printf("hello, I'm %s, I'm %d\n", m.name, m.age)
-}
-
-func (m *myType) set() {
-	m.age = m.age + 5
+func noni(n int) {
+	fmt.Println(15 * n)
 }
 
 func main() {
-	a := myType{name: "Axel", age: 25}
-	a.get()
-	a.set()
-	a.get()
+	noni(5)
 }
