b3cc0e97d535952628e824fe640b86b54c42c382
Safin <ilnursafin35@gmail.com>
2026-06-24 20:42:21 +0500

    the 24th of June

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -1,24 +1,13 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
-	var (
-		flagString string
-		flagInt    int
-		flagBool   bool
-	)
-
-	flag.StringVar(&flagString, "stringvar", "default", "an example string var")
-	flag.IntVar(&flagInt, "intval", 42, "an example int var")
-	flag.BoolVar(&flagBool, "boolval", false, "an example bool var")
-
-	flag.Parse()
-
-	fmt.Println("String var:", flagString)
-	fmt.Println("Int var:", flagInt)
-	fmt.Println("Boolean var:", flagBool)
+	// os.Args содержит все аргументы командной строки,
+	// включая имя исполняемого файла
+	args := os.Args
+	fmt.Println(args)
 }
