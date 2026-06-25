f24f3f993ebdb7055723df60f5455b3d5c248885
Safin <ilnursafin35@gmail.com>
2026-06-25 11:55:54 +0500

    25th June

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
 func main() {
-	// os.Args содержит все аргументы командной строки,
-	// включая имя исполняемого файла
-	args := os.Args
-	fmt.Println(args)
+	// Определение флагов
+	verbose := flag.Bool("v", false, "verbose output")
+	port := flag.Int("port", 8080, "server port")
+
+	// Парсинг флагов
+	flag.Parse()
+
+	// Аргументы командной строки (не флаги)
+	args := flag.Args()
+
+	fmt.Printf("Verbose: %v\n", *verbose)
+	fmt.Printf("Port: %d\n", *port)
+	fmt.Printf("Arguments: %v\n", args)
+	fmt.Printf("All command line args (including flags): %v\n", os.Args)
 }
