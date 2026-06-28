736aacc1cb3ab2550dff2f3c3633ca3e020f13c5
Safin <ilnursafin35@gmail.com>
2026-06-28 16:57:20 +0500

    June 28th Go runtime

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -1,26 +1,10 @@
 package main
 
 import (
-	"bytes"
-	"log"
-	"os/exec"
+	"fmt"
+	"runtime"
 )
 
 func main() {
-	cmd := exec.Command("grep", "go")
-
-	var stdin bytes.Buffer // Создаем и записываем данные в STDIN команды
-	cmd.Stdin = &stdin
-	stdin.Write([]byte("golang\npython\njava\n"))
-
-	var stdout, stderr bytes.Buffer // Буферы для STDOUT и STDERR
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-
-	err := cmd.Run() // Запускаем команду
-	if err != nil {
-		log.Fatalf("Ошибка выполнения: %s\nStderr: %s", err, stderr.String())
-	}
-
-	log.Printf("Stdout: %s", stdout.String())
+	fmt.Println(runtime.NumCPU())
 }
