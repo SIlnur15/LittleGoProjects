25979004b55145c4637ffa9515c9a206d4b05894
Safin <ilnursafin35@gmail.com>
2026-08-03 01:20:12 +0500

    August 3rd Fan-In pattern

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -1,15 +1,34 @@
 package main
 
-import "fmt"
-
-func routiner(cch chan int) {
-	cch <- 15
-	cch <- 25
-}
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
-	channel := make(chan int)
-	go routiner(channel)
-	fmt.Println(<-channel)
-	fmt.Println(<-channel)
+	manyWritersOneReader()
+}
+
+func manyWritersOneReader() {
+	ch := make(chan int)
+	var wg sync.WaitGroup
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			ch <- id
+		}(i)
+	}
+
+	// Отдельная горутина для ожидания и закрытия канала
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	// Читатель успешно завершит цикл, когда канал закроется
+	for id := range ch {
+		fmt.Println("Получен ID:", id)
+	}
 }
