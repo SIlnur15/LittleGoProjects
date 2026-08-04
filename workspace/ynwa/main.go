00d125a69d4bd0f51d53acdc17c83d1d5314fe0d
Safin <ilnursafin35@gmail.com>
2026-08-05 04:29:38 +0500

    August 5th already...

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -2,23 +2,32 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
-	synchronization()
-}
+	var wg sync.WaitGroup
+	taskChannel := make(chan string) // Создаем небуферизированный канал для передачи задач
 
-func synchronization() {
-	ch := make(chan struct{}) // Канал для синхронизации
-
-	go func() {
-		fmt.Println("Горутина начинает работу")
-		time.Sleep(time.Second)
-		fmt.Println("Горутина завершила работу")
-		ch <- struct{}{} // Сигнал о завершении
+	wg.Add(1)
+	go func() { // Горутина-исполнитель
+		defer wg.Done()
+		for task := range taskChannel {
+			// Получение задачи (блокируется, пока не будет отправки)
+			fmt.Printf("start of processing: %s\n", task)
+			time.Sleep(1 * time.Second) // Имитация работы
+			fmt.Printf("is over: %s\n", task)
+		}
 	}()
 
-	<-ch // Ожидание завершения
-	fmt.Println("Основная горутина продолжает работу")
+	tasks := []string{"task 1", "task 2", "task 3"}
+	for _, task := range tasks { // Отправляем 3 задачи последовательно
+		fmt.Printf("sending %s...\n", task)
+		taskChannel <- task
+		fmt.Printf("was sent: %s\n", task)
+	}
+
+	close(taskChannel) // Закрываем канал после отправки всех задач
+	wg.Wait()
 }
