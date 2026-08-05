468a2f66279125e76bf9b4fc41a6e640eda5f4fe
Safin <ilnursafin35@gmail.com>
2026-08-06 01:20:53 +0500

    August 6th unidirectional chanel

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -5,35 +5,28 @@ import (
 	"time"
 )
 
+// producer принимает канал только для записи (chan<-)
+func producer(ch chan<- int) {
+	for i := 0; i < 5; i++ {
+		ch <- i // отправляем значения в канал
+		fmt.Printf("Отправлено: %d\n", i)
+		time.Sleep(time.Second) // задержка для наглядности
+	}
+	close(ch) // закрываем канал после отправки всех значений
+}
+
+// consumer принимает канал только для чтения (<-chan)
+func consumer(ch <-chan int) {
+	for num := range ch {
+		fmt.Printf("Получено: %d\n", num)
+	}
+}
+
 func main() {
-	ch1 := make(chan int)
-	ch2 := make(chan int)
+	ch := make(chan int) // создаем двунаправленный канал
 
-	go func() { // Горутина 1: пытается отправить в ch1, потом прочитать из ch2
-		for {
-			select {
-			case ch1 <- 1:
-				fmt.Println("Горутина 1 отправила в ch1")
-			case <-ch2:
-				fmt.Println("Горутина 1 получила из ch2")
-			}
-			time.Sleep(100 * time.Millisecond)
-		}
-	}()
+	go producer(ch) // запускаем producer в горутине
+	consumer(ch)    // consumer работает в main горутине
 
-	go func() { // Горутина 2: пытается отправить в ch2, потом прочитать из ch1
-		for {
-			select {
-			case ch2 <- 2:
-				fmt.Println("Горутина 2 отправила в ch2")
-			case <-ch1:
-				fmt.Println("Горутина 2 получила из ch1")
-			}
-			time.Sleep(100 * time.Millisecond)
-		}
-	}()
-
-	// Даем livelock'у поработать
-	time.Sleep(time.Second)
-	fmt.Println("Livelock продолжается...")
+	fmt.Println("Готово!")
 }
