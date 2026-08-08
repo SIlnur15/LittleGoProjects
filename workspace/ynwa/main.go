d706fac293fc12397633b571dba55cfef4863d53
Safin <ilnursafin35@gmail.com>
2026-08-08 19:03:39 +0500

    August 8th, tee pattern

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -5,26 +5,45 @@ import (
 	"time"
 )
 
-func writeEvery(msg string, seconds time.Duration) <-chan string {
-	messages := make(chan string)
-	go func() {
-		for {
-			time.Sleep(seconds)
-			messages <- msg
+func tee(input <-chan int, outputs ...chan<- int) {
+	for v := range input {
+		for _, output := range outputs {
+			output <- v
 		}
-	}()
-	return messages
+	}
+
+	// Закрываем все выходные каналы после завершения
+	for _, output := range outputs {
+		close(output)
+	}
 }
 
 func main() {
-	messagesFromA := writeEvery("Tick", 1*time.Second)
-	messagesFromB := writeEvery("Tock", 3*time.Second)
-	for {
-		select {
-		case msg1 := <-messagesFromA:
-			fmt.Println(msg1)
-		case msg2 := <-messagesFromB:
-			fmt.Println(msg2)
+	in := make(chan int)
+	out1 := make(chan int)
+	out2 := make(chan int)
+
+	// Запускаем tee в отдельной горутине
+	go tee(in, out1, out2)
+
+	// Запускаем потребителей
+	go func() {
+		for v := range out1 {
+			fmt.Println("out1:", v)
 		}
+	}()
+
+	go func() {
+		for v := range out2 {
+			fmt.Println("out2:", v)
+		}
+	}()
+
+	// Отправляем данные в входной канал
+	for i := 0; i < 5; i++ {
+		in <- i
 	}
+
+	close(in)
+	time.Sleep(time.Second) // Даем время для обработки
 }
