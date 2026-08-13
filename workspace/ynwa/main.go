005cc8a1071fcafe49380130283df4df365ba4f0
Safin <ilnursafin35@gmail.com>
2026-08-13 23:58:35 +0500

    August 14th, Forcible goroutine termination, combination with sync.WaitGroup

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -1,36 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
-func countTo(max int) (<-chan int, func()) {
-	ch := make(chan int)
-	done := make(chan struct{})
+func workerWithWG(wg *sync.WaitGroup, stop <-chan struct{}, id int) {
+	defer wg.Done()
 
-	cancel := func() {
-		close(done)
-	}
-
-	go func() {
-		for i := 0; i < max; i++ {
-			select {
-			case <-done:
-				return
-			default:
-				ch <- i
-			}
+	for {
+		select {
+		case <-stop:
+			fmt.Printf("Worker %d received stop signal\n", id)
+			return
+		default:
+			fmt.Printf("Worker %d working...\n", id)
+			time.Sleep(time.Duration(id) * 500 * time.Millisecond)
 		}
-		close(ch)
-	}()
-	return ch, cancel
+	}
 }
 
 func main() {
-	ch, cancel := countTo(10)
-	for i := range ch {
-		if i > 3 {
-			break
-		}
-		fmt.Println(i)
+	var wg sync.WaitGroup
+	stop := make(chan struct{})
+
+	// Запускаем горутины
+	for i := 1; i <= 3; i++ {
+		wg.Add(1)
+		go workerWithWG(&wg, stop, i)
 	}
-	cancel()
+
+	// Даем поработать 5 секунд
+	time.Sleep(5 * time.Second)
+
+	// Останавливаем все горутины
+	close(stop)
+
+	// Ждем завершения всех горутин
+	wg.Wait()
+	fmt.Println("All workers stopped")
 }
