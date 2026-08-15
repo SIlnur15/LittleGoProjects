6fbbe24798b82eabcd51dd7d013075ce0b00a852
Safin <ilnursafin35@gmail.com>
2026-08-15 23:40:46 +0500

    August 15th, data race

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -1,22 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+var counter int
 
 func main() {
-	ch1 := make(chan int) // небуферизованный канал
-	ch2 := make(chan int) // небуферизованный канал
-
-	go func() {
-		ch1 <- 1 // отправка в ch1
-		fmt.Println("Sent 1 to ch1")
-		<-ch2 // получение из ch2
-	}()
-
-	go func() {
-		ch2 <- 2 // отправка в ch2
-		fmt.Println("Sent 2 to ch2")
-		<-ch1 // получение из ch1
-	}()
-
-	select {} // бесконечное ожидание
+	var wg sync.WaitGroup
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			counter++ // Небезопасный инкремент
+		}()
+	}
+	wg.Wait()
+	fmt.Println("Final counter:", counter)
 }
