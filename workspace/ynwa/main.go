162959286badf0f57a705369fd1787b89af1c9b5
Safin <ilnursafin35@gmail.com>
2026-08-17 14:45:29 +0500

    August 17th, sync.Cond Signal

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -2,17 +2,36 @@ package main
 
 import (
 	"fmt"
-	"sync/atomic"
+	"sync"
+	"time"
+)
+
+var (
+	mu      sync.Mutex
+	cond    *sync.Cond = sync.NewCond(&mu)
+	counter int
 )
 
 func main() {
-	var score int32 = 100 // Начальное значение
+	for i := 0; i < 3; i++ {
+		go listener(i)
+	}
 
-	success := atomic.CompareAndSwapInt32(&score, 100, 150) // Успешный случай: Проверяем: "Если score равен 100, то сделай его 150"
-	fmt.Println("Успешно?", success)                        // Выведет: true
-	fmt.Println("Score:", score)                            // Выведет: 150
+	for i := 0; i < 100; i++ {
+		time.Sleep(1 * time.Second)
+		mu.Lock()
+		counter++
+		fmt.Println("Counter:", counter)
+		cond.Signal() // отправляем сигнал одной горутине
+		mu.Unlock()
+	}
+}
 
-	success2 := atomic.CompareAndSwapInt32(&score, 100, 200) // Неуспешный случай: Проверяем: "Если score равен 100, то сделай его 200". Но score уже равен 150! Условие не выполнено.
-	fmt.Println("Успешно?", success2)                        // Выведет: false
-	fmt.Println("Score:", score)                             // Выведет: 150 (значение НЕ изменилось)
+func listener(id int) {
+	for {
+		mu.Lock()
+		cond.Wait()
+		fmt.Println("Listener:", id, " Counter:", counter)
+		mu.Unlock()
+	}
 }
