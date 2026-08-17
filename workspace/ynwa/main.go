4a8d077e655b776561a77edb3eb51a4df240ce89
Safin <ilnursafin35@gmail.com>
2026-08-17 18:11:58 +0500

    August 17th, sync.Once

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -6,38 +6,16 @@ import (
 )
 
 func main() {
-	var m sync.Map
+	var once sync.Once
 
-	// Добавление пары ключ-значение
-	m.Store("greeting", "hello")
-
-	// Получение значения по ключу
-	if value, ok := m.Load("greeting"); ok {
-		fmt.Println(value.(string)) // -> hello
+	// Функция, которая будет выполнена только один раз.
+	greet := func() {
+		fmt.Println("Hello, World!")
 	}
 
-	// Получение значения по отсутствующему ключу
-	if _, ok := m.Load("nonexistent"); !ok {
-		fmt.Println("значение не существует") // -> значение не существует
-	}
+	// Вызов метода Do для выполнения функции greet.
+	once.Do(greet)
 
-	// Замена значения по ключу
-	m.Store("greeting", "hi")
-
-	// Удаление пары ключ-значение по ключу
-	m.Delete("greeting")
-
-	// Перебор всех пар ключ-значение и вывод их на экран
-	m.Store("key1", "value1")
-	m.Store("key2", "value2")
-	m.Range(func(key, value interface{}) bool {
-		fmt.Println(key.(string) + ": " + value.(string))
-		return true // продолжаем итерацию
-	})
-
-	// Замена значения по ключу (аналогично Store, но возвращает предыдущее значение, если оно существует)
-	previous, loaded := m.Swap("key3", "value3")
-	if loaded {
-		fmt.Println(previous.(string)) // -> value1 (предыдущее значение ключа key3, если оно существует)
-	}
+	// Повторный вызов метода Do не приведет к повторному выполнению функции greet.
+	once.Do(greet)
 }
