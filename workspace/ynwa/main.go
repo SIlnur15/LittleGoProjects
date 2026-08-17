396fc6f48ed60ae768dbc2aed01ecd8cde29a5d0
Safin <ilnursafin35@gmail.com>
2026-08-17 17:19:56 +0500

    August 17th, sync.Map

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -3,35 +3,41 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
-)
-
-var (
-	mu      sync.Mutex
-	cond    *sync.Cond = sync.NewCond(&mu)
-	counter int
 )
 
 func main() {
-	for i := 0; i < 3; i++ {
-		go listener(i)
+	var m sync.Map
+
+	// Добавление пары ключ-значение
+	m.Store("greeting", "hello")
+
+	// Получение значения по ключу
+	if value, ok := m.Load("greeting"); ok {
+		fmt.Println(value.(string)) // -> hello
 	}
 
-	for i := 0; i < 100; i++ {
-		time.Sleep(1 * time.Second)
-		mu.Lock()
-		counter++
-		fmt.Println("Counter:", counter)
-		cond.Broadcast() // отправляем сигнал всем горутинам
-		mu.Unlock()
+	// Получение значения по отсутствующему ключу
+	if _, ok := m.Load("nonexistent"); !ok {
+		fmt.Println("значение не существует") // -> значение не существует
 	}
-}
 
-func listener(id int) {
-	for {
-		mu.Lock()
-		cond.Wait()
-		fmt.Println("Listener:", id, " Counter:", counter)
-		mu.Unlock()
+	// Замена значения по ключу
+	m.Store("greeting", "hi")
+
+	// Удаление пары ключ-значение по ключу
+	m.Delete("greeting")
+
+	// Перебор всех пар ключ-значение и вывод их на экран
+	m.Store("key1", "value1")
+	m.Store("key2", "value2")
+	m.Range(func(key, value interface{}) bool {
+		fmt.Println(key.(string) + ": " + value.(string))
+		return true // продолжаем итерацию
+	})
+
+	// Замена значения по ключу (аналогично Store, но возвращает предыдущее значение, если оно существует)
+	previous, loaded := m.Swap("key3", "value3")
+	if loaded {
+		fmt.Println(previous.(string)) // -> value1 (предыдущее значение ключа key3, если оно существует)
 	}
 }
