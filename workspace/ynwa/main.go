5767f8ce460f3e73c33e96d67992b661f7747fe5
Safin <ilnursafin35@gmail.com>
2026-08-27 20:01:28 +0500

    August 27th, I'm at home again

--- workspace/ynwa/main.go
+++ workspace/ynwa/main.go
@@ -1,25 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
+import "fmt"
 
 func main() {
-	const n = 5
-	results := make([]string, n)
-	var wg sync.WaitGroup
-
-	for i := range n {
-		wg.Go(func() {
-			results[i] = fmt.Sprintf("Привет из горутины %d", i) // пишем в свой индекс
-		})
-	}
-
-	wg.Wait()
-
-	for _, msg := range results {
-		fmt.Println(msg)
-	}
-	fmt.Println("Все горутины завершились!")
+	fmt.Println("Hello my dear son Haidaric !!!")
 }
