3166e88a0fd189d32c7702edba278ca12dfa086c
Safin <ilnursafin35@gmail.com>
2026-09-10 23:55:04 +0500

    September 10th, func GetMovie added

--- workspace/gorilla-demo/main.go
+++ workspace/gorilla-demo/main.go
@@ -22,6 +22,17 @@ func GetMovies(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(movies)
 }
 
+func GetMovie(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, item := range movies {
+		if item.Title == params["title"] {
+			json.NewEncoder(w).Encode(&item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Movie{})
+}
+
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/movies", GetMovies).Methods("GET")
