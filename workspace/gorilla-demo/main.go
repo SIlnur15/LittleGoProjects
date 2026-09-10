029d52367c6363af0e88e4bffd6e40c29d24e99e
Safin <ilnursafin35@gmail.com>
2026-09-10 22:01:13 +0500

    September 11th, project with gorilla phase 2

--- workspace/gorilla-demo/main.go
+++ workspace/gorilla-demo/main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-    "encoding/json"
-    "log"
-    "net/http"
+	"encoding/json"
+	"log"
+	"net/http"
 
-    "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 )
+
+type Movie struct {
+	Title  string `json:"title,omitempty"`
+	Rating int    `json:"rating,omitempty"`
+}
+
+var movies = []Movie{
+	{"Побег", 9},
+	{"The Godfather", 9},
+} // Создадим слайс с тестовыми данными
+
+func GetMovies(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(movies)
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/movies", GetMovies).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
